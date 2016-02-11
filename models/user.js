8c9f1f16bc56484b665da2fccd362b793cc49e71
dmitryame <dmitry@custommode.com>
2016-02-10 19:18:43 -0500

    passing tests

--- models/user.js
+++ models/user.js
@@ -4,28 +4,15 @@ var massive = require("massive");
 
 var db = require('../consts').DB;
 
-
-var SCHEMA = ['userName' , 'password', 'email'];
-
 var User = function(properties) {
   _.assign(this, properties);
 }
 
-User.prototype.save = function *() {
-  var results = [],
-      data    = _.pick(this, SCHEMA);
-
-  // if(this.id) {
-  console.log('connected: ' + db);
-  db.users.save({email : "new@example.com"}, function(err,inserted){
-    console.log("error: " + err);
-    //the new record with the ID
-    console.log('inserted: ' + inserted);
-  });
-
-
-  // }
-
+User.prototype.save = function () {
+  var inserted = db.users.saveSync(this);
+    if(!this.id) {
+      this.id = inserted.id; // assign newly generated id to the object
+    }
 }
 
 module.exports = User;
--- tests/user.js
+++ tests/user.js
@@ -4,25 +4,23 @@ var assert = require('assert'),
 require('co-mocha');
 
 describe('User model testing', function() {
+
   it('should create a user', function *() {
     var user = new User();
     assert.equal(typeof user, 'object');
-  })
+  });
 
   it('should store properties passed when instantiated', function *() {
-    var userName, iser;
-    userName = 'james';
-    user = new User({userName: userName});
+    var userName = 'james';
+    var user = new User({userName: userName});
     assert.equal(user.userName, userName);
-  })
+  });
 
-  it('should assign as id after being saved', function *() {
-    var userName, password, user;
-    userName = 'james';
-    password = 'secret';
-    var user = new User({userName: userName, password: password});
-    yield user.save();
+  it('should assign id after being saved', function *() {
+    var user = new User({userName: 'james', password: 'secret', email: 'e@example.com'});
+    user.save();
+    console.log(user);
     assert(user.id);
-  })
+  });
 
 });
