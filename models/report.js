67e76fcad7ba05c1e8c05883ae3bad89804a49f6
dmitryame <dmitry@custommode.com>
2016-03-12 16:48:37 -0500

    added year sort order

--- models/report.js
+++ models/report.js
@@ -10,8 +10,8 @@ var Report = function(properties) {}
 // all years available on report for for particular user
 Report.prototype.yearsForUser = function (user_id) {
   // console.log(user_id);
-  let results = db.runSync("select distinct(extract(year from checked_in_at)) from checkins where user_id = $1 ORDER BY date_part", [user_id]);
-  // console.log(results);
+  let results = db.runSync("select distinct(extract(year from checked_in_at)) from checkins where user_id = $1 ORDER BY date_part desc", [user_id]);
+  console.log(results);
   return results;
 }
 
