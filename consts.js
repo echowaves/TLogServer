8ba68a9f246dfa04861a322be5c5f9e36a585839
dmitryame <dmitry@custommode.com>
2016-04-26 18:45:59 -0400

    cleaning up

--- consts.js
+++ consts.js
@@ -10,8 +10,13 @@ module.exports = Object.freeze({
     API_VERSION_IOS:     '1.0.0',
     DB: massive.connectSync(
       {connectionString:
-        "postgres://" + process.env.TLOG_DB_USER + ":" + process.env.TLOG_DB_PASS + "@" + process.env.TLOG_DB_HOST  + "/" + process.env.TLOG_DB_NAME}),
-    SEND_GRID_API_USER: process.env.TLOG_SEND_GRID_API_USER || "tlog",
-    SEND_GRID_API_PASSWORD: process.env.TLOG_SEND_GRID_API_PASSWORD || "echoKuku90",
-    TL_HOST: process.env.TLOG_TL_HOST || "http://tlog.us:3000"
+        "postgres://" +
+        process.env.TLOG_DB_USER + ":" +
+        process.env.TLOG_DB_PASS + "@" +
+        process.env.TLOG_DB_HOST  + "/" +
+        process.env.TLOG_DB_NAME
+      }),
+    SEND_GRID_API_USER: process.env.TLOG_SEND_GRID_API_USER ,
+    SEND_GRID_API_PASSWORD: process.env.TLOG_SEND_GRID_API_PASSWORD ,
+    TL_HOST: process.env.TLOG_HOST
 });
