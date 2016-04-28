fd81a078289b0408ece300b39cedca7913320b10
dmitryame <dmitry@custommode.com>
2016-04-27 21:58:29 -0400

    debugging

--- rundev.sh
+++ rundev.sh
@@ -2,6 +2,6 @@ export TLOG_DB_HOST=localhost
 export TLOG_DB_NAME=tlog_dev
 export TLOG_DB_PASS=root
 export TLOG_DB_USER=root
-export TLOG_HOST=http://localhost:3000
+export TLOG_HOST=http://192.168.1.145:3000
 
 node app.js
