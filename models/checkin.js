aeb74b82372502fc93d32b03a071e882fff5cdea
dmitryame <dmitry@custommode.com>
2016-08-26 10:38:48 -0400

    WIP: checking refactoring

--- models/checkin.js
+++ models/checkin.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var db = require('../consts').DB;
 
 
@@ -7,78 +5,71 @@ var _ = require('lodash');
 
 // var db = require('../consts').DB;
 
-var Checkin = function(properties) {
-  _.assign(this, properties);
-}
 
-
-// set id to the checkin object, call load to populate the rest of the properties
-Checkin.prototype.load = function (callback) {
-  var that = this;
-  db.checkins.findOne({id:this.id}, function(err, checkinRes) {
-    if(err) {
-      console.log("error Checkin.prototype.load");
-      console.log(err);
+export function load(params) {
+  return function(callback) {
+    db.checkins.findOne(params, function(err, checkinRes) {
+      if(err) {
+        console.log("error Checkin.prototype.load");
+        console.log(err);
+        callback(err, checkinRes);
+        return;
+      };
       callback(err, checkinRes);
-      return;
-    };
-    _.assign(that, checkinRes);
-    callback(err, checkinRes);
-  });
+    });
+  }
 }
 
-// set email address  to the checkin object (test covered only by route test)
-Checkin.prototype.loadAll = function (page_number, page_size, callback) {
-  if(this.email == null)          {
-    callback("email is missin", null);
-    return;
-  };
-  if(page_number == null)   { page_number = '0';};
-  if(page_size == null)     { page_size = '100';};
+//(test covered only by route test)
+export function loadAll(email, page_number, page_size) {
+  return function(callback) {
+    if(email == null)          {
+      callback("email is missin", null);
+      return;
+    };
+    if(page_number == null)   { page_number = '0';};
+    if(page_size == null)     { page_size = '100';};
 
-  db.run("SELECT c.id, c.email, c.user_id, c.checked_in_at, c.duration, c.action_code_id, a.code, a.description FROM checkins c INNER JOIN action_codes a ON c.action_code_id = a.id WHERE c.email=$1 ORDER BY c.checked_in_at desc LIMIT $2 OFFSET $3", [this.email, parseInt(page_size), parseInt(page_number) * parseInt(page_size)],
-  function(err, checkinsRes) {
-    if(err) {
-      console.log("error Checkin.prototype.loadAll");
-      console.log(err);
+    db.run("SELECT c.id, c.email, c.user_id, c.checked_in_at, c.duration, c.action_code_id, a.code, a.description FROM checkins c INNER JOIN action_codes a ON c.action_code_id = a.id WHERE c.email=$1 ORDER BY c.checked_in_at desc LIMIT $2 OFFSET $3", [email, parseInt(page_size), parseInt(page_number) * parseInt(page_size)],
+    function(err, checkinsRes) {
+      if(err) {
+        console.log("error Checkin.prototype.loadAll");
+        console.log(err);
+        callback(err, checkinsRes);
+        return;
+      };
       callback(err, checkinsRes);
-      return;
-    };
-    callback(err, checkinsRes);
-  });
+    });
+  }
 }
-
 
 // upsert checkin
-Checkin.prototype.save = function (callback) {
-  var that = this;
-  db.checkins.save(_.omit(that, _.keys(_.pickBy(that,_.isFunction))), function(err, res){
-    if(err) {
-      console.log("error Checkin.prototype.save");
-      console.log(err);
+export function save(params) {
+  return function(callback) {
+    db.checkins.save(params, function(err, res){
+      if(err) {
+        console.log("error Checkin.save");
+        console.log(err);
+        callback(err, res);
+        return;
+      }
       callback(err, res);
-      return;
-    }
-    if(res) {
-      _.assign(that, res);
-    }
-    callback(err, res);
-  });
+    });
+  }
 }
 
 
 //delete checkin
-Checkin.prototype.delete = function (callback) {
-  db.checkins.destroy({id: this.id}, function(err, res){
-    if(err) {
-      console.log("error Checkin.prototype.delete");
-      console.log(err);
+export function destroy(params) {
+  return function(callback) {
+    db.checkins.destroy(params, function(err, res){
+      if(err) {
+        console.log("error Checkin.delete");
+        console.log(err);
+        callback(err, res);
+        return;
+      }
       callback(err, res);
-      return;
-    }
-    callback(err, res);
-  });
+    });
+  }
 }
-
-
-module.exports = Checkin;
--- tests/models/checkin.js
+++ tests/models/checkin.js
@@ -1,11 +1,10 @@
-'use strict';
-
 process.env.NODE_ENV = 'test'
 const app = require('../../app.js');
 
 
-var assert   = require('assert'),
-    Checkin = require('../../models/checkin');
+var assert   = require('assert');
+
+import * as Checkin from '../../models/checkin';
 
 require('co-mocha');
 var uuid = require('uuid');
@@ -16,31 +15,6 @@ var moment = require('moment');
 
 describe('Checkin model testing', function() {
 
-  it('should create an checkin', function *() {
-    var checkin = new Checkin();
-    assert.equal(typeof checkin, 'object');
-  });
-
-  it('should store properties passed when instantiated', function *() {
-    var email = uuid.v4() + "@example.com";
-    var user_id = 1;
-    var checked_in_at = moment().toDate();
-    var duration = moment.duration(3, 'hours').asSeconds();
-    var action_code_id = 1;
-    var checkin =
-      new Checkin({
-        email: email,
-        user_id: user_id,
-        checked_in_at: checked_in_at,
-        duration: duration,
-        action_code_id: action_code_id
-      });
-    assert.equal(checkin.email, email);
-    assert.equal(checkin.user_id, user_id);
-    assert.equal(checkin.checked_in_at, checked_in_at);
-    assert.equal(checkin.duration, duration);
-    assert.equal(checkin.action_code_id, action_code_id);
-  });
 
   it('should assign id after being saved', function *() {
     var email = uuid.v4() + "@example.com";
@@ -49,14 +23,13 @@ describe('Checkin model testing', function() {
     var duration = moment.duration(3, 'hours').asSeconds();
     var action_code_id = 1;
     var checkin =
-      new Checkin({
-        email: email,
-        user_id: user_id,
-        checked_in_at: checked_in_at,
-        duration: duration,
-        action_code_id: action_code_id
-      });
-    yield checkin.save.bind(checkin);
+    yield Checkin.save({
+      email: email,
+      user_id: user_id,
+      checked_in_at: checked_in_at,
+      duration: duration,
+      action_code_id: action_code_id
+    });
     assert(checkin.id);
   });
 
@@ -67,19 +40,17 @@ describe('Checkin model testing', function() {
     var duration = moment.duration(3, 'hours').asSeconds();
     var action_code_id = 1;
     var checkin =
-      new Checkin({
-        email: email,
-        user_id: user_id,
-        checked_in_at: checked_in_at,
-        duration: duration,
-        action_code_id: action_code_id
-      });
-    yield checkin.save.bind(checkin);
+    yield Checkin.save({
+      email: email,
+      user_id: user_id,
+      checked_in_at: checked_in_at,
+      duration: duration,
+      action_code_id: action_code_id
+    });
     var checkin1 =
-      new Checkin({
-        id: checkin.id
-      });
-    yield checkin1.load.bind(checkin1);
+    yield Checkin.load({
+      id: checkin.id
+    });
     assert.deepEqual(checkin, checkin1);
   });
 
